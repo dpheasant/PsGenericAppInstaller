498b44847a233df358f805eeeb7c92d4ae792fd8
David T. Pheasant <david.pheasant@bluemetal.com>
2018-06-07 23:00:26 -0400

    Adding tests for utils module

--- modules/inputHandlers/inputHandlers.Tests.ps1
+++ modules/inputHandlers/inputHandlers.Tests.ps1
@@ -1,6 +1,6 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.'
-import-module "$here"
+import-module -Force "$here"
 
 
 Describe "InputHandlers: Get-Targets" {
