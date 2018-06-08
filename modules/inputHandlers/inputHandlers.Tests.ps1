09bc9022bf84b04e2f6bd67cebf13f533fc2d0df
David T. Pheasant <david.pheasant@bluemetal.com>
2018-06-07 20:51:56 -0400

    Adding input csv processing functionality

--- modules/inputHandlers/inputHandlers.Tests.ps1
+++ modules/inputHandlers/inputHandlers.Tests.ps1
@@ -1,9 +1,17 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', ''
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psm1'
 import-module "$here\$sut"
 
-Describe "inputHandlers" {
-    It "does something useful" {
+Describe "InputHandlers: Get-Targets" {
+    It "throws an error when the file does not exist" {
+        $true | Should -Be $false
+    }
+
+    It "throws an error when the file is formatted incorrectly" {
+        $true | Should -Be $false
+    }
+
+    It "returns a list of targets" {
         $true | Should -Be $false
     }
 }
