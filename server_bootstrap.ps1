8001213542487fedeca068d0c4b8a2824d9459ff
David T. Pheasant <david.pheasant@bluemetal.com>
2018-06-14 15:21:27 -0700

    Comitting server bootstrap with Geoff's changes

--- server_bootstrap.ps1
+++ server_bootstrap.ps1
@@ -8,12 +8,4 @@ $results = start-installation `
         -installScript 'C:\scripts\PsGenericAppInstaller\clientInstall_bootstrap.ps1'
 $sut  = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psm1'
 
-import-module -Force (resolve-path "$here/$sut")
-
-$results = start-installation `
-        -targetsFile './modules/inputHandlers/test_targets.csv' `
-        -sitesFile   './modules/inputHandlers/test_sites.csv' `
-        -siteCommandsFile './modules/inputHandlers/test_siteCommands.csv' `
-        -installScript 'clientInstall_bootstrap.ps1'
-
 $results | Write-StatusReport -Filename 'results.csv'
